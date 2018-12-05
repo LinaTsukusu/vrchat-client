2344ce6ffca1286822a3567fad8447221f5f425e
neverclear86 <neverclear86sab@gmail.com>
2018-12-05 15:11:22 +0900

    実装し始め TS楽しいね

---
+++ src/modules/api-module.ts
@@ -0,0 +1,47 @@
+import VrcApi from '../vrc-api'
+
+
+export default class ApiModule {
+  private vrc: VrcApi
+
+  constructor(vrc) {
+    this.vrc = vrc
+  }
+
+  protected async get(url: string, params: object = {}) {
+    let p = Object.assign({
+      apiKey: this.vrc.apiKey,
+      authToken: this.vrc.token,
+    }, params)
+    console.log(p)
+    return await this.vrc.api.get(url, {
+      params: p,
+    })
+  }
+
+  protected async delete(url: string) {
+    return await this.vrc.api.delete(url, {
+      params: {
+        apiKey: this.vrc.apiKey,
+        authToken: this.vrc.token,
+      },
+    })
+  }
+
+  private async request(method: ('post' | 'put'), url: string, body: object = null) {
+    return await this.vrc.api[method](url, body, {
+      params: {
+        apiKey: this.vrc.apiKey,
+        authToken: this.vrc.token,
+      },
+    })
+  }
+
+  protected async post(url: string, body: object = null) {
+    return await this.request('post', url, body)
+  }
+
+  protected async put(url: string, body: object = null) {
+    return await this.request('put', url, body)
+  }
+}
